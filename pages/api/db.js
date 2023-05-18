0f8144fa422bd3a4be6bc91e54f7b1f52557e768
Diohandres Hernandez <diohandres1703@hotmail.com>
2023-05-17 20:14:56 -0400

    add-mysql2-package

--- pages/api/db.js
+++ pages/api/db.js
@@ -1,18 +1,21 @@
-import mysql from 'serverless-mysql';
-const db = mysql({
-  config: {
-    host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_PORT,
-    database: process.env.MYSQL_DATABASE,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD
-  }
+import mysql from 'mysql2';
+const db = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  database: process.env.MYSQL_DATABASE,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD
 });
 export default async function excuteQuery({ query, values }) {
   try {
-    const results = await db.query(query, values);
-    await db.end();
-    return results;
+    db.query(query, values, (err, result, fields) => {
+      console.log(err);
+      console.log(result); // results contains rows returned by server
+      console.log(fields);
+      return result;
+
+    });
+
   } catch (error) {
     return { error };
   }
