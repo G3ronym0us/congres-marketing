905f03e31c6952b2d753028b44e3510cb606f7cf
Diohandres Hernandez <diohandres1703@hotmail.com>
2023-05-26 17:24:07 -0400

    Pendientes

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -9,28 +9,24 @@ const CarouselComponent = () => {
                       process.env.NEXT_PUBLIC_URL +
                       "images/carousel1.jpg"
                     } alt="Imagen 1" />
-        <p className="legend">Imagen 1</p>
       </div>
       <div>
         <img src={
                       process.env.NEXT_PUBLIC_URL +
                       "images/carousel2.jpg"
                     } alt="Imagen 2" />
-        <p className="legend">Imagen 2</p>
       </div>
       <div>
         <img src={
                       process.env.NEXT_PUBLIC_URL +
                       "images/carousel3.jpg"
                     } alt="Imagen 3" />
-        <p className="legend">Imagen 3</p>
       </div>
       <div>
         <img src={
                       process.env.NEXT_PUBLIC_URL +
                       "images/carousel1.jpg"
                     } alt="Imagen 4" />
-        <p className="legend">Imagen 3</p>
       </div>
     </Carousel>
   );
