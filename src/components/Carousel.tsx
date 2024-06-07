ca8f560670b957746bf64d2c6e60017371742975
Diohandres <diohandres1703@hotmail.com>
2024-06-07 18:04:04 -0400

    update-carouse

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,5 +1,8 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // Importar estilos del carrusel
-import { Carousel } from "react-responsive-carousel"; // Importar componente del carrusel
+import { Carousel as OriginalCarousel } from "react-responsive-carousel"; // Importar componente del carrusel
+
+// Sobrescribir la definición de tipo para Carousel
+const Carousel: any = OriginalCarousel;
 
 const images = [
   "carousel2.jpg",
@@ -13,18 +16,20 @@ const images = [
 
 const CarouselComponent = () => {
   return (
-    <Carousel>
-      {images.map((image, index) => {
-        return (
-          <div key={index}>
-            <img
-              src={process.env.NEXT_PUBLIC_URL + "images/" + image }
-              alt={image}
-            />
-          </div>
-        );
-      })}
-    </Carousel>
+    <>
+      <Carousel>
+        {images.map((image, index) => {
+          return (
+            <div key={index}>
+              <img
+                src={process.env.NEXT_PUBLIC_URL + "images/" + image }
+                alt={image}
+              />
+            </div>
+          );
+        })}
+      </Carousel>
+    </>
   );
 };
 
