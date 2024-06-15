39b2f01d51daf05fcd479653dacfb0e79ff665ec
Diohandres <diohandres1703@hotmail.com>
2024-06-15 01:50:32 -0400

    fix-resend-email

--- pages/api/admin/tickets/resend/[id].ts
+++ pages/api/admin/tickets/resend/[id].ts
@@ -98,7 +98,7 @@ async function resendEmail(user: Ticket) {
   const customFont = await pdfDoc.embedFont(fontBytes);
 
   // Agrega la imagen
-  const imageUrl = process.env.NEXT_PUBLIC_URL + "images/pdf-img-template.png";
+  const imageUrl = process.env.NEXT_PUBLIC_URL + "images/pdf-img-template.jpg";
 
   const imageResponse = await fetch(imageUrl);
   if (!imageResponse.ok) {
@@ -106,7 +106,7 @@ async function resendEmail(user: Ticket) {
   }
 
   const imageBuffer = await imageResponse.arrayBuffer();
-  const image = await pdfDoc.embedPng(imageBuffer);
+  const image = await pdfDoc.embedJpg(imageBuffer);
   page.drawImage(image, {
     x: 0,
     y: 0,
@@ -114,11 +114,46 @@ async function resendEmail(user: Ticket) {
     height: page.getHeight(),
   });
 
+  let imageTextUrl =
+    process.env.NEXT_PUBLIC_URL + "images/pdf-text-general.png";
+  switch (user.type) {
+    case "Diamante":
+      imageTextUrl =
+        process.env.NEXT_PUBLIC_URL + "images/pdf-text-diamante.png";
+      break;
+    case "Oro":
+      imageTextUrl = process.env.NEXT_PUBLIC_URL + "images/pdf-text-oro.png";
+      break;
+    case "Platea Derecha":
+      imageTextUrl = process.env.NEXT_PUBLIC_URL + "images/pdf-text-platea.png";
+      break;
+    case "Platea Izquierda":
+      imageTextUrl = process.env.NEXT_PUBLIC_URL + "images/pdf-text-platea.png";
+      break;
+    default:
+      imageTextUrl =
+        process.env.NEXT_PUBLIC_URL + "images/pdf-text-general.png";
+      break;
+  }
+
+  const imageTextResponse = await fetch(imageTextUrl);
+  if (!imageTextResponse.ok) {
+    throw new Error("Failed to fetch the image");
+  }
+
+  // const imgeTextBuffer = await imageTextResponse.arrayBuffer();
+  // const imageText = await pdfDoc.embedPng(imgeTextBuffer);
+  // page.drawImage(imageText, {
+  //   x: 0,
+  //   y: 0,
+  //   width: page.getWidth(),
+  //   height: page.getHeight(),
+  // });
+
   // Agrega el código QR
   const qrCodeUrl = "https://cnmpcolombia.com/ticket/" + user.uuid;
   const qrCodeDataUrl = await QRCode.toDataURL(qrCodeUrl);
   const qrCodeImage = await pdfDoc.embedPng(qrCodeDataUrl);
-  console.log(page.getWidth(), page.getHeight());
   page.drawImage(qrCodeImage, {
     x: 383,
     y: 66,
@@ -126,44 +161,28 @@ async function resendEmail(user: Ticket) {
     height: 150,
   });
 
-  page.drawText(user.name, {
-    x: 77, // Posición horizontal del texto en la página
-    y: 572, // Posición vertical del texto en la página
-    size: 38, // Tamaño de fuente del texto
-    font: customFont, // Fuente del texto (puedes cargar otras fuentes)
-    color: rgb(1, 1, 1), // Color del texto (en este caso, negro)
-  });
-
-  page.drawText(user.lastname, {
-    x: 77, // Posición horizontal del texto en la página
-    y: 532, // Posición vertical del texto en la página
-    size: 28, // Tamaño de fuente del texto
-    font: customFont, // Fuente del texto (puedes cargar otras fuentes)
-    color: rgb(1, 1, 1), // Color del texto (en este caso, negro)
-  });
-
   page.drawText(user.document, {
-    x: 77, // Posición horizontal del texto en la página
-    y: 504, // Posición vertical del texto en la página
-    size: 28, // Tamaño de fuente del texto
-    font: customFont, // Fuente del texto (puedes cargar otras fuentes)
-    color: rgb(1, 1, 1), // Color del texto (en este caso, negro)
-  });
-
-  page.drawText(user.type.toUpperCase(), {
-    x: 170, // Posición horizontal del texto en la página
-    y: 450, // Posición vertical del texto en la página
-    size: 14, // Tamaño de fuente del texto
-    font: customFont, // Fuente del texto (puedes cargar otras fuentes)
-    color: rgb(1, 1, 1), // Color del texto (en este caso, negro)
+    x: 77,
+    y: 570,
+    size: 28,
+    font: customFont,
+    color: rgb(1, 1, 1),
   });
 
   page.drawText(`${user.row} ${user.number}`, {
-    x: 170, // Posición horizontal del texto en la página
-    y: 431, // Posición vertical del texto en la página
-    size: 14, // Tamaño de fuente del texto
-    font: customFont, // Fuente del texto (puedes cargar otras fuentes)
-    color: rgb(1, 1, 1), // Color del texto (en este caso, negro)
+    x: 430,
+    y: 440,
+    size: 18,
+    font: customFont,
+    color: rgb(1, 1, 1),
+  });
+
+  page.drawText(`${user.type}`, {
+    x: 180,
+    y: 440,
+    size: 18,
+    font: customFont,
+    color: rgb(1, 1, 1),
   });
 
   const pdfBytes = await pdfDoc.save();
@@ -177,43 +196,31 @@ async function resendEmail(user: Ticket) {
     port: 465,
     secure: true,
     auth: {
-      user: "Info2@cnmpcolombia.com",
+      user: "info2@cnmpcolombia.com",
       pass: "3st0esProd123*",
     },
   });
 
   const mailOptions = {
     from: "info2@cnmpcolombia.com",
-    to: user.email,
+    to: "yesidguinand2012@gmail.com",
     subject: "Confirmación de Participación CNMP",
     text: `
-            ¡Enhorabuena!
-            Haz asegurado tu cupo para participar del Congreso Nacioal de Marketing Político - Colombia 2023.
-            
-            Recuerda asistir puntual a las conferencias y actividades programadas, y seguir las instrucciones de nuestro personal logístico.
-
-            En el Congreso Nacional de Marketing Político queremos asegurarnos de que tu experiencia sea la mejor, incluyendo tu alojamiento. 
-
-Para tu descanso después de un día lleno de aprendizaje, nos complace recomendarte el Hotel Bari, reconocido por su excelencia en servicio y comodidad, su ubicación estratégica a tres cuadras de la Universidad Autónoma de Bucaramanga, facilitan el acceso a nuestro evento. 
-
-En él recibirás tarifas exclusivas, acreditando con tu entrada que participarás en el Congreso Nacional de Marketing Político. 
-
-¡Te invitamos a vivir una experiencia excepcional en el Congreso Nacional de Marketing Político y disfrutar de la comodidad y hospitalidad del Hotel Bari! 
-
-Tarifa de alojamiento por noche en habitación estándar queen o twin (dos camas) $210.000 + IVA.
-Incluye desayuno en barra tipo buffet.
-No incluye seguro hotelero de $8.100 por persona por noche
-            
-            Lugar: Auditorio mayor Carlos Gómez Albarracín UNAB, Bucaramanga, Colombia.
-            
-            Fecha: 14 y 15 de julio de 2023.
-            
-            Si requieres información, escríbenos a cnmmcolombia@gmail.com o a nuestro WhatsApp 3181200000
-            
-            ¡Te esperamos!`,
+              ¡Enhorabuena!
+              Haz asegurado tu cupo para participar del Congreso Nacioal de Marketing Político - Colombia 2023.
+              
+              Recuerda asistir puntual a las conferencias y actividades programadas, y seguir las instrucciones de nuestro personal logístico.
+              
+              Lugar: Auditorio mayor Carlos Gómez Albarracín UNAB, Bucaramanga, Colombia.
+              
+              Fecha: 14 y 15 de julio de 2023.
+              
+              Si requieres información, escríbenos a cnmmcolombia@gmail.com o a nuestro WhatsApp 3181200000
+              
+              ¡Te esperamos!`,
     attachments: [
       {
-        filename: "image_with_qr.pdf",
+        filename: "QR-" + user.document + ".pdf",
         content: pdfBuffer,
       },
     ],
