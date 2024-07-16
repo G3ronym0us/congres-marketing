258321ec84d939a13cba3850512c42021145537b
Diohandres <diohandres1703@hotmail.com>
2024-07-16 11:59:47 -0400

    test-middleware-error

--- middleware.ts
+++ middleware.ts
@@ -2,20 +2,6 @@ import { NextRequest, NextResponse } from "next/server";
 import { jwtVerify } from "jose";
 
 export async function middleware(request: NextRequest) {
-  try {
-    const pathname = request.nextUrl.pathname;
-    const jwt = request.cookies.get("token");
-
-    if (pathname.includes("admin/dashboard")) {
-      if (!jwt)
-        return NextResponse.redirect(new URL("/admin/auth", request.url));
-
-      const jwtToken = jwt.value;
-      
-      return NextResponse.next();
-    }
-  } catch (error) {
-    console.log(error);
-    return NextResponse.redirect(new URL("/admin/auth", request.url));
-  }
+  console.log("Middleware called for:", request.nextUrl.pathname);
+  return NextResponse.next();
 }
